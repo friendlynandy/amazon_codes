05f60dbcbd626e3be94803f13f5dc01ca9717a1b
safeerk <safeerk@gmail.com>
2015-06-23 10:57:12 +0530

    Safeers Latest Push

--- image.php
+++ image.php
@@ -1,20 +1,26 @@
 <?php
 
-$gameable_id = $_GET["user_id"];
-
-//echo $gameable_id;
-
+$user_id = $_GET["user_id"];
+if (strlen($user_id)<3)
+{
+$user_id = str_pad($user_id, 3, "0", STR_PAD_LEFT);
+}
 require_once('connection.php');
-$result = pg_query($dbconn3, "select avatar_file_name from users where id=$gameable_id");
+$result = pg_query($dbconn3, "select avatar_file_name from users where id=$user_id");
 
-$num = pg_numrows($result);
-
+if($rows[0]==0)
+{
+$ar = array("http://savanna.sportslion.com/leaderboard/app/user.png");	
+}
+else
+{
 $rows = array();
 while($r = pg_fetch_assoc($result))
 {
 	$rows[] = $r;
 }
-$ar = array("http://sportslion.production.s3-eu-west-1.amazonaws.com/users/avatars/000/000/".$gameable_id ."/large/".$rows[0][avatar_file_name]);
+$ar = array("http://sportslion.production.s3-eu-west-1.amazonaws.com/users/avatars/000/000/".$user_id ."/large/".$rows[0][avatar_file_name]);
+}
 echo json_encode($ar, JSON_FORCE_OBJECT); 
 pg_close($dbconn3);
 ?>
