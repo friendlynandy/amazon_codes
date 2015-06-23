34dc28c54c1bca33baee346a5e50ce4de870e3a0
raghunadh2006ster <raghunadh2006ster@me.com>
2015-06-23 11:58:10 +0530

    Update image.php

--- image.php
+++ image.php
@@ -14,7 +14,7 @@ while($r = pg_fetch_assoc($result))
 }
 if ($rows[0][avatar_file_name]=="")
 {
-$ar = array("http://www.sportslion.com/assets/avatar10b523aaf2f6e9d8aea4d6eb56c4e2db7.png");
+$ar = array("http://www.sportslion.com/assets/avatar-0b523aaf2f6e9d8aea4d6eb56c4e2db7.png");
 }
 else{
 $ar = array("http://sportslion.production.s3-eu-west-1.amazonaws.com/users/avatars/000/000/".$user_id ."/large/".$rows[0][avatar_file_name]);
