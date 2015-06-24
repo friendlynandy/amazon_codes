6c72689ab600761ba5147ec38b4bea78953224bd
raghunadh2006ster <raghunadh2006ster@me.com>
2015-06-24 10:44:34 +0530

    Update password.php

--- password.php
+++ password.php
@@ -1,10 +1,9 @@
 <?php
 $dbconn3 = pg_connect("host=ec2-79-125-7-27.eu-west-1.compute.amazonaws.com port=5552 dbname=d39ubt6siqrqmf user=u5rve30b5so8gv password=peqhcba53fc1d4qrebs5ok7b22");
 //$username=preg_replace('/\s+/', '', $_GET['username']);
-$username = str_replace('', '%20', $_GET['username']);
+//$username = str_replace('', '%20', $_GET['username']);
+$username = $_GET(['username']);
 $password = $_GET(['password']);
-
-echo $username;
 
 //pgsql get his encrypted_password for the username
 $encrypted_password = pg_query($dbconn3,"select id,encrypted_password from users where username = '$username'");
