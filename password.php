275c15b456cfa72e9cf7f55982c2156084017e1c
raghunadh2006ster <raghunadh2006ster@me.com>
2015-06-24 10:43:14 +0530

    Update password.php

--- password.php
+++ password.php
@@ -2,7 +2,7 @@
 $dbconn3 = pg_connect("host=ec2-79-125-7-27.eu-west-1.compute.amazonaws.com port=5552 dbname=d39ubt6siqrqmf user=u5rve30b5so8gv password=peqhcba53fc1d4qrebs5ok7b22");
 //$username=preg_replace('/\s+/', '', $_GET['username']);
 $username = str_replace('', '%20', $_GET['username']);
-$password = $_GET['password']);
+$password = $_GET(['password']);
 
 echo $username;
 
