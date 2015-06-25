6c3dd8a7368fe51957b70dd6eec65d5f6b3c7c13
raghunadh2006ster <raghunadh2006ster@me.com>
2015-06-25 12:56:30 +0530

    Update and rename mail.php to signup.php

--- signup.php
+++ signup.php
@@ -78,19 +78,19 @@ else if(isset($username))
 {
 require_once('connection.php');
 $code = str_random();
-/*
+
 $options = [
     'cost' => 10,
 ];
 $hash = password_hash('$encrypted_password', PASSWORD_BCRYPT, $options);
 $bodytag = str_replace("$2y", "$2a", $hash);
-*/
 
+/*
 $salt = substr(strtr(base64_encode(openssl_random_pseudo_bytes(22)), '+', '.'), 0, 22);
 // 2y is the bcrypt algorithm selector, see http://php.net/crypt
 // 12 is the workload factor (around 300ms on my Core i7 machine), see http://php.net/crypt
 $bodytag = crypt('$encrypted_password', '$2a$10$' . $salt);
-
+*/
 
 $result = pg_query($dbconn3, "INSERT INTO users (full_name,encrypted_password,username,email,tos_agreement,balance,given_points,confirmation_token,created_at,updated_at) VALUES('$full_name','$bodytag','$username','$email','$tos_agreement','20','20','$code','$timestamp_new','$timestamp_new')");
 
