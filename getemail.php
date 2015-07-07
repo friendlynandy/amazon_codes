23aaa829c57396cf7e802248d9cb90e508355994
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-07 16:00:24 +0530

    Update getemail.php

--- getemail.php
+++ getemail.php
@@ -5,7 +5,7 @@ $email = $_GET["email"];
 if(isset($_GET["invitebyemail"]))
 {
   $invite_email = $_GET["invitebyemail"];
-  $result = pg_query($dbconn3, "select id,username from users where email = '$invite_email'");
+  $result = pg_query($dbconn3, "select id,username,email from users where email = '$invite_email'");
 $num = pg_numrows($result);
 $rows = array();
 while($r = pg_fetch_assoc($result))
