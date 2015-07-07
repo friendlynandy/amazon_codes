bea45000c8f72f1d907a6acbf9b9818c34fa346a
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-07 15:56:42 +0530

    Update getemail.php

--- getemail.php
+++ getemail.php
@@ -1,15 +1,11 @@
 <?php 
-
 require_once('connection.php');
 $email = $_GET["email"];
-$result = pg_query($dbconn3, "select email from users where email = '$email'");
 
-if(isset($_GET["invitebyemail"])
+if(isset($_GET["invitebyemail"]))
 {
-$inivite_email = $_GET["invitebyemail"];
-echo $invite_email;
-}
-
+  $invite_email = $_GET["invitebyemail"];
+  $result = pg_query($dbconn3, "select id,username from users where email = '$invite_email'");
 $num = pg_numrows($result);
 $rows = array();
 while($r = pg_fetch_assoc($result))
@@ -17,5 +13,18 @@ while($r = pg_fetch_assoc($result))
 	$rows[] = $r;
 }
 echo json_encode($rows);
+}
+else
+{
+$result = pg_query($dbconn3, "select email from users where email = '$email'");
+$num = pg_numrows($result);
+$rows = array();
+while($r = pg_fetch_assoc($result))
+{
+	$rows[] = $r;
+}
+echo json_encode($rows);
+}
 pg_close($dbconn3);
+
 ?>
