ae328da0011f58e0558d68d5beeabae0b14d0c20
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-10 13:47:14 +0530

    Update displayroars.php

--- displayroars.php
+++ displayroars.php
@@ -6,16 +6,20 @@ $row = $_GET["id"];
 //echo $row;
 
 //printf ($row[0]);
-$result = pg_query($dbconn3, "select a.id as duel_games_id, f.username,f.balance,f.id,j.name as competition_name,a.bet_cost,e.name as user_team, 
-c.name as opponent_team,a.status,g.result,h.name,CASE WHEN a.user_id ='$row' THEN 'waiting'   ELSE 'respond'
+$result = pg_query($dbconn3, "select a.id as duel_games_id, a.main as competitable_type,a.sec as sec_compet_type, f.full_name,f.username,f.balance,f.id,j.name as competition_name,n.name as compet_name,a.bet_cost,e.name as user_team, 
+c.name as opponent_team,a.status,g.result,h.name as team_sport,k.name as opponent_player,l.name as user_player,m.name as player_sport,CASE WHEN a.user_id ='$row' THEN 'waiting'   ELSE 'respond'
        END from
-(
-SELECT id, user_id,gameable_id, opponent_id,bet_cost,user_competitor_id,opponent_competitor_id,status FROM
- duel_games where user_id = '$row'
+       (
+select * from (SELECT b.competitable_type as main,c.competitable_type as sec,a.id, a.user_id,a.gameable_id, a.opponent_id,a.bet_cost,a.user_competitor_id,a.opponent_competitor_id,a.status FROM
+ duel_games a 
+ left join competitors b on a.opponent_competitor_id = b.id
+ left join competitors c on a.user_competitor_id = c.id where user_id = '$row'
 UNION ALL
-SELECT id, user_id,gameable_id, user_id,bet_cost,opponent_competitor_id,user_competitor_id,status FROM duel_games
- where opponent_id = '$row'
-)as a
+SELECT b.competitable_type,c.competitable_type,a.id, a.user_id,a.gameable_id, a.user_id,a.bet_cost,a.opponent_competitor_id,a.user_competitor_id,a.status FROM duel_games a 
+left join competitors b on a.user_competitor_id = b.id 
+left join competitors c on a.opponent_competitor_id = c.id 
+ where opponent_id = '$row') as list order by list.id desc
+) as a
 left join competitors b on a.opponent_competitor_id = b.id
 left join teams c on b.competitable_id = c.id
 left join competitors d on a.user_competitor_id = d.id
@@ -25,7 +29,11 @@ left join user_duel_games g on a.id = g.duel_game_id and g.user_id = '$row'
 left join sports h on c.sport_id = h.id
 left join matches i on a.gameable_id = i.id
 left join competitions j on i.competition_id = j.id
- order by a.id desc
+left join players k on b.competitable_id = k.id
+left join players l on d.competitable_id = l.id
+left join sports m on l.sport_id = m.id
+left join competitions n on a.gameable_id = n.id
+order by a.id desc
 ");
 
 $num = pg_numrows($result);
@@ -35,7 +43,43 @@ while($r = pg_fetch_assoc($result))
 {
 	$rows[] = $r;
 }
-echo json_encode($rows); 
-
+foreach ($rows as $key => $value) 
+{
+		if ($rows[$key][username]=="")
+		{
+		$rows[$key][username]="?";
+		}
+		
+		if ($rows[$key][full_name]==null)
+		{
+		$rows[$key][full_name]="?";
+		}
+		
+		
+		if ($rows[$key][competitable_type]=="")
+		{
+		$rows[$key][competitable_type]="?";
+		}
+		
+		if ($rows[$key][user_player]=="")
+		{
+		$rows[$key][user_player]="?";
+		}
+		
+		if ($rows[$key][opponent_player]=="")
+		{
+		$rows[$key][opponent_player]="?";
+		}
+		if ($rows[$key][player_sport]=="")
+		{
+		$rows[$key][player_sport]="Sport of the Day";
+		}
+		if ($rows[$key][player_sport]=="" && $rows[$key][team_sport]=="")
+		{
+		$rows[$key][player_sport]="Sport of the Day";
+		}
+		
+}
+echo json_encode($rows);
 
 ?>
