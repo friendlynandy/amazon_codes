1864ded96076ca348d2d813ff49efe86f3e49941
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-15 10:45:53 +0530

    Update editprofile.php

--- editprofile.php
+++ editprofile.php
@@ -1,7 +1,19 @@
 <?php
 require_once('connection.php');
 $id = $_GET["id"];
+
 $result = pg_query($dbconn3, "select  full_name,username from users where id = '$id'");
+if(isset[$_GET["username"]])
+{
+$username = $_GET["username"];	
+$result = pg_query($dbconn3, "select  full_name,username from users where username != '$username'");
+while($r = pg_fetch_assoc($result))
+{
+	$rows[] = $r;
+}
+echo json_encode($rows);
+}
+
 while($r = pg_fetch_assoc($result))
 {
 	$rows[] = $r;
