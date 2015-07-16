cd0aee0c38f09a1a648ba96512e5666dd3b7f42b
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-16 11:42:27 +0530

    Update getusername.php

--- getusername.php
+++ getusername.php
@@ -5,14 +5,33 @@ $id = $_GET["id"];
 $result = pg_query($dbconn3, "select username,balance from users where username = '$username'");
 if(isset($id))
 {
-$result = pg_query($dbconn3, "select username,balance from users where id = '$id'");	
-}
+$result = pg_query($dbconn3, "select full_name,username,balance from users where id = '$id'");	
 $num = pg_numrows($result);
-pg_close($dbconn3);
+$rows = array();
+while($r = pg_fetch_assoc($result))
+{
+	$rows[] = $r;
+}
+foreach ($rows as $key => $value) 
+{
+if ($rows[$key][full_name]==null)
+		{
+		$rows[$key][full_name]="?";
+		}
+
+}
+echo json_encode($rows);
+}
+else
+{
+$result = pg_query($dbconn3, "select username,balance from users where username = '$username'");
+$num = pg_numrows($result);
 $rows = array();
 while($r = pg_fetch_assoc($result))
 {
 	$rows[] = $r;
 }
 echo json_encode($rows);
+}
+pg_close($dbconn3);
 ?>
