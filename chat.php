cbb3b96bbb1cb5e4aadb16ec415c5257db04af56
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-29 10:49:44 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -5,8 +5,13 @@ $sent_user_id = $_GET['opponent_id'];
 $message = $_GET['message'];
 $date_new = new DateTime();
 $timestamp = $date_new->format('Y-m-d H:i:s');
+if(isset($_GET["emailid"] && $_GET['opponent_id'] && $_GET['message']))
+{
+$duel_games_id = $_GET['duel_games_id'];
+$sent_user_id = $_GET['opponent_id'];
+$message = $_GET['message'];
 $result = pg_query($dbconn3, "INSERT INTO  chats (duel_games_id,sent_user_id,message,created_at,updated_at) VALUES('$duel_games_id','$sent_user_id','$message','$timestamp','$timestamp')");
-
+}
 
 $result1 = pg_query($dbconn3, "select * from chats where duel_games_id = '$duel_games_id' order by id desc");
 $num = pg_numrows($result1);
