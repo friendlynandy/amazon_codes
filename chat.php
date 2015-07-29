8b1e9d031b21d04786cb5ed8320b219f4bb12c2f
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-29 18:19:24 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -13,14 +13,6 @@ $duel_games_id = $_GET['duel_games_id'];
 $sent_user_id = $_GET['opponent_id'];
 $message = $_GET['message'];
 $result = pg_query($dbconn3, "INSERT INTO  chats (duel_games_id,sent_user_id,message,created_at,updated_at) VALUES('$duel_games_id','$sent_user_id','$message','$timestamp','$timestamp')");
-}
-if(isset($_GET['duel_games_id']) && isset($_GET['x']) && isset($_GET['y']))
-{
-$duel_games_id = $_GET['duel_games_id'];
-$x = $_GET["x"];
-$y = $_GET["y"];
-$result1 = pg_query($dbconn3, "select * from chats where duel_games_id = '$duel_games_id' limit $y offset $x");
-}
 $num = pg_numrows($result1);
 $rows = array();
 while($r = pg_fetch_assoc($result1))
@@ -28,5 +20,20 @@ while($r = pg_fetch_assoc($result1))
 	$rows[] = $r;
 }
 echo json_encode($rows);
+}
+if(isset($_GET['duel_games_id']) && isset($_GET['x']) && isset($_GET['y']))
+{
+$duel_games_id = $_GET['duel_games_id'];
+$x = $_GET["x"];
+$y = $_GET["y"];
+$result1 = pg_query($dbconn3, "select * from chats where duel_games_id = '$duel_games_id' limit $y offset $x");
+$num = pg_numrows($result1);
+$rows = array();
+while($r = pg_fetch_assoc($result1))
+{
+	$rows[] = $r;
+}
+echo json_encode($rows);
+}
 pg_close($dbconn3);
 ?>
