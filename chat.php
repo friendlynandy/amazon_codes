6a1e2670db0f02ee96ce6f9bc58f9d05ae97d6d8
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-29 10:56:47 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -5,7 +5,7 @@ $sent_user_id = $_GET['opponent_id'];
 $message = $_GET['message'];
 $date_new = new DateTime();
 $timestamp = $date_new->format('Y-m-d H:i:s');
-if(isset($_GET['duel_games_id']))
+if(isset($_GET['duel_games_id']) && isset($_GET['opponent_id']) && isset($_GET['message']))
 {
 $duel_games_id = $_GET['duel_games_id'];
 $sent_user_id = $_GET['opponent_id'];
