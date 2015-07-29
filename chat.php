0db5def89eaf4721faf8ef6d3657f4c3bb7c5e26
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-29 18:21:04 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -13,13 +13,6 @@ $duel_games_id = $_GET['duel_games_id'];
 $sent_user_id = $_GET['opponent_id'];
 $message = $_GET['message'];
 $result = pg_query($dbconn3, "INSERT INTO  chats (duel_games_id,sent_user_id,message,created_at,updated_at) VALUES('$duel_games_id','$sent_user_id','$message','$timestamp','$timestamp')");
-$num = pg_numrows($result);
-$rows = array();
-while($r = pg_fetch_assoc($result))
-{
-	$rows[] = $r;
-}
-echo json_encode($rows);
 }
 if(isset($_GET['duel_games_id']) && isset($_GET['x']) && isset($_GET['y']))
 {
