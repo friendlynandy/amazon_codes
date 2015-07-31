c666354a32cff2303b193a5bfa46b4aee6273a94
raghunadh2006ster <raghunadh2006ster@me.com>
2015-07-31 15:09:51 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -13,6 +13,103 @@ $duel_games_id = $_GET['duel_games_id'];
 $sent_user_id = $_GET['opponent_id'];
 $message = $_GET['message'];
 $result = pg_query($dbconn3, "INSERT INTO  chats (duel_games_id,sent_user_id,message,created_at,updated_at) VALUES('$duel_games_id','$sent_user_id','$message','$timestamp','$timestamp')");
+$result = pg_query($dbconn3,"select ios_token_id,ios_notification_badge from push_notifiers");
+$rows = array();
+
+while($r = pg_fetch_assoc($result))
+
+{
+
+	$rows[] = $r;
+
+
+
+}
+
+
+
+$message = stripslashes($_POST['message']);
+
+foreach ($rows as $key => $value)
+
+{
+
+$notificationBadge = $rows[$key]["ios_notification_badge"];
+
+$notification = $notificationBadge+1;
+
+
+
+$payload = '{
+
+
+
+                 "aps" :
+
+                 
+
+                        {  "alert" : "'.$message.'",
+
+                           "badge" : '.$notification.',
+
+                           "sound" : "default"
+
+        
+
+                        }
+
+                        
+
+            }';
+
+            
+
+            
+
+$ctx = stream_context_create();
+
+stream_context_set_option($ctx,'ssl','local_cert','finalsportslion.pem');
+
+stream_context_set_option($ctx,'ssl','passphrase','');
+
+$fp = stream_socket_client('ssl://gateway.sandbox.push.apple.com:2195',$err,$errstr,60,STREAM_CLIENT_CONNECT,$ctx);
+
+if(!$fp)
+
+{
+
+    print "Failed to connect $err $errstrn";
+
+    return;
+
+}
+
+else
+
+{
+
+   print "Notification sent";
+
+}
+
+
+
+	$deviceToken = $rows[$key]["ios_token_id"];
+
+   $msg = chr (0) . pack("n",32) . pack('H*', str_replace(' ', '', $deviceToken)) . pack("n",strlen($payload)) . $payload;
+
+   print "sending message :" . $payload . "n";
+
+   fwrite($fp, $msg);
+
+
+
+   $result = pg_query($dbconn3,"update push_notifiers set ios_notification_badge = '$notification' where ios_token_id = '$deviceToken'");
+
+ }
+
+fclose($fp);
+
 }
 if(isset($_GET['duel_games_id']) && isset($_GET['x']) && isset($_GET['y']))
 {
