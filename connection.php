35702477ed9f9fe1658919fa7f53be53e134ad2b
raghunadh2006ster <raghunadh2006ster@me.com>
2015-08-23 12:44:07 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn_string= "host=ec2-79-125-7-27.eu-west-1.compute.amazonaws.com port=5552 dbname=d39ubt6siqrqmf user=u5rve30b5so8gv password=peqhcba53fc1d4qrebs5ok7b22";
+$conn_string= "host=ec2-54-217-247-223.eu-west-1.compute.amazonaws.com port=5442 dbname=d39ubt6siqrqmf user=u5rve30b5so8gv password=peqhcba53fc1d4qrebs5ok7b22";
 
 $dbconn3 = pg_pconnect($conn_string);
 if (!$dbconn3 ) {
