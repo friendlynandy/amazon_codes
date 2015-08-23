5cfff9b1e5e1719d557bd32f4e962035913d722c
raghunadh2006ster <raghunadh2006ster@me.com>
2015-08-23 13:38:47 +0530

    Update nav.php

--- leaderboard/include/nav.php
+++ leaderboard/include/nav.php
@@ -3,14 +3,14 @@
 			<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
 
 <div class="menu-sports-menu-container"><ul id="menu-sports-menu" class="menu nav-menu">
-<li  <?php echo ($page == 'overall') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://savanna.sportslion.com/leaderboard/">Overall</a></li>
-<li  <?php echo ($page == 'am-football') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://savanna.sportslion.com/leaderboard/am-football.php">American Football</a></li>
-<li  <?php echo ($page == 'cricket') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://savanna.sportslion.com/leaderboard/cricket.php">Cricket</a></li>
-<li  <?php echo ($page == 'er-football') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://savanna.sportslion.com/leaderboard/eurofootball.php">European Football</a></li>
-<li  <?php echo ($page == 'figure') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://savanna.sportslion.com/leaderboard/figure-skating.php">Figure Skating</a></li>
-<li  <?php echo ($page == 'tennis') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://savanna.sportslion.com/leaderboard/tennis.php">Tennis</a></li>
-<li  <?php echo ($page == 'ice-hockey') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://savanna.sportslion.com/leaderboard/ice-hockey.php">Ice Hockey</a></li>
-<li  <?php echo ($page == 'basketball') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://savanna.sportslion.com/leaderboard/basketball.php">Basketball</a></li>
+<li  <?php echo ($page == 'overall') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://54.191.199.139/amazon_codes/leaderboard/">Overall</a></li>
+<li  <?php echo ($page == 'am-football') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://54.191.199.139/amazon_codes/am-football.php">American Football</a></li>
+<li  <?php echo ($page == 'cricket') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://54.191.199.139/amazon_codes/cricket.php">Cricket</a></li>
+<li  <?php echo ($page == 'er-football') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://54.191.199.139/amazon_codes/eurofootball.php">European Football</a></li>
+<li  <?php echo ($page == 'figure') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://54.191.199.139/amazon_codes/figure-skating.php">Figure Skating</a></li>
+<li  <?php echo ($page == 'tennis') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://54.191.199.139/amazon_codes/tennis.php">Tennis</a></li>
+<li  <?php echo ($page == 'ice-hockey') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://54.191.199.139/amazon_codes/ice-hockey.php">Ice Hockey</a></li>
+<li  <?php echo ($page == 'basketball') ? "class='menu-item current-menu-item'" : ""; ?>><a href="http://54.191.199.139/amazon_codes/basketball.php">Basketball</a></li>
 
 </ul></div>
 
