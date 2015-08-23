6358c4b523de1fcff4cafae67396df09e82389f9
raghunadh2006ster <raghunadh2006ster@me.com>
2015-08-23 12:56:39 +0530

    Update connect.php

--- leaderboard/include/connect.php
+++ leaderboard/include/connect.php
@@ -1,6 +1,6 @@
 <?php
 // Connecting, selecting database
-$conn_string= "host=ec2-79-125-7-27.eu-west-1.compute.amazonaws.com port=5552 dbname=d39ubt6siqrqmf user=u5rve30b5so8gv password=peqhcba53fc1d4qrebs5ok7b22";
+$conn_string= "host=ec2-54-217-247-223.eu-west-1.compute.amazonaws.com port=5442 dbname=d39ubt6siqrqmf user=u5rve30b5so8gv password=peqhcba53fc1d4qrebs5ok7b22";
 $dbconn = pg_pconnect($conn_string);
 if (!$dbconn) 
 {
