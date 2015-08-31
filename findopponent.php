265dd2bc26806d9d9addae24bc3148ad61b146d8
raghunadh2006ster <raghunadh2006ster@me.com>
2015-08-31 11:31:21 +0530

    Update findopponent.php

--- findopponent.php
+++ findopponent.php
@@ -2,7 +2,7 @@
 require_once('connection.php');
 $competition_id = $_GET["competition_id"];
 $opponentcompetitor_id = $_GET["opponent_competitor_id"];
-//$userselection_id = $_GET[""];
+$user_id = $_GET["user_id"];
 //$row = pg_fetch_row($id);
 $result = pg_query($dbconn3, "select d.username, user_id,count(user_id) from(
 select a.id, user_id,a.status, user_competitor_id as selection from duel_games a
@@ -14,7 +14,7 @@ select a.id,opponent_id,a.status, opponent_competitor_id as selection from duel_
 left join matches b on a.gameable_id = b.id
 left join competitions c on b.competition_id = c.id
 where c.id = '$competition_id' and a.status = 'accepted') as list left join users d on 
-list.user_id = d.id where selection = '$opponentcompetitor_id' group by user_id,d.username order by count desc limit 1");
+list.user_id = d.id where user_id != '$user_id' group by user_id,d.username order by count desc limit 1;
 
 $num = pg_numrows($result);
 pg_close();
