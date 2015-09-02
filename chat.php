dd13a2879a652fb1d9048467f20d3978efd82a4e
raghunadh2006ster <raghunadh2006ster@me.com>
2015-09-02 13:49:57 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -90,7 +90,7 @@ $duel_games_id = $_GET['duel_games_id'];
 $x = $_GET["x"];
 $y = $_GET["y"];
 $userid = $_GET["user_id"];
-echo $userid
+echo $userid;
 var_dump($userid);
 pg_query($dbconn3, "update chat_notifiers set notification_badge = 0 where duel_games_id = '$duel_games_id' and user_id = '$userid'");
 $result1 = pg_query($dbconn3, "select * from chats where duel_games_id = '$duel_games_id' order by id desc limit $y offset $x");
