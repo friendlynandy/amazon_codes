8e4b51e4270c9e3551e94804005a3c4a15532be7
raghunadh2006ster <raghunadh2006ster@me.com>
2015-09-07 12:00:51 +0530

    Update testmail.php

--- testmail.php
+++ testmail.php
@@ -1,10 +1,12 @@
 <?php
-// The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
-
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
-
-// Send
-mail('raghunadh2006ster@outlook.com', 'My Subject', $message);
+$mailto="xxxxxxxx@gmail.com";
+ $subject="mail test";
+ $content="test";
+ $result=mail($mailto, $subject, $content);
+ if($result){
+    echo "mail success";
+ }else  {
+    error_log($mailto, 0);  
+    echo "mail fail";
+ }
 ?>
