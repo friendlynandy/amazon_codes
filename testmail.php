bc47e2be1c264fd5f0f5e956a964cfcdee6c0b2b
raghunadh2006ster <raghunadh2006ster@me.com>
2015-09-07 13:25:17 +0530

    Update testmail.php

--- testmail.php
+++ testmail.php
@@ -10,7 +10,7 @@ function amazonSesEmail($to, $subject, $message)
     ));
 $fromEmailAddress = 'raghunadh2006ster@outlook.com';
  $amazonSes->verify_email_address($fromEmailAddress);
-    $response = $amazonSes->send_email('raghunadh2006ster@outlook.com',
+    $response = $amazonSes->send_email('raghunadh2006ster@me.com',
         array('ToAddresses' => array($to)),
         array(
             'Subject.Data' => $subject,
