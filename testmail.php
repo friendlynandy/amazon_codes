1e077d18bb26b18772781f399fff567e0506cf83
raghunadh2006ster <raghunadh2006ster@me.com>
2015-09-07 12:50:53 +0530

    Update testmail.php

--- testmail.php
+++ testmail.php
@@ -4,20 +4,20 @@ include('AWSSDKforPHP/sdk.class.php');
 function amazonSesEmail($to, $subject, $message)
 {
     $amazonSes = new AmazonSES(array(
-        'key' => AKIAIUBQUFTDXXNOUODQ,
-        'secret' => 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
+        'key' => 'AKIAIUBQUFTDXXNOUODQ',
+        'secret' => '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'
     ));
  
-    // $response = $amazonSes->send_email(AWS_SES_FROM_EMAIL,
-    //     array('ToAddresses' => array($to)),
-    //     array(
-    //         'Subject.Data' => $subject,
-    //         'Body.Text.Data' => $message,
-    //     )
-    // );
-    // if (!$response->isOK())
-    // {
-    //     // handle error
-    // }
+    $response = $amazonSes->send_email(AWS_SES_FROM_EMAIL,
+        array('ToAddresses' => array($to)),
+        array(
+            'Subject.Data' => $subject,
+            'Body.Text.Data' => $message,
+        )
+    );
+    if (!$response->isOK())
+    {
+        // handle error
+    }
 }
 ?>
