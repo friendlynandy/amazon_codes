874ee59804de17c38634e9548614bfd8f74e08a6
raghunadh2006ster <raghunadh2006ster@me.com>
2015-09-10 11:08:29 +0530

    Update updatestattus_duelgame.php

--- updatestattus_duelgame.php
+++ updatestattus_duelgame.php
@@ -1,11 +1,10 @@
 <?php
 require_once('connection.php'); 
-$user_id = $_POST[''];
-$duel_games_id = $_POST[''];
-$publish_status = 'f';
+$user_id = $_POST['user_id'];
+$duel_games_id = $_POST['duel_games_id'];
 $date_new = new DateTime();
 $timestamp = $date_new->format('Y-m-d H:i:s');
-$result = pg_query($dbconn3,"INSERT INTO  chats (duel_game_id,user_id,publish_status,created_at,updated_at) VALUES('$duel_games_id','$user_id','$publish_status','$timestamp','$timestamp')");
+$result = pg_query($dbconn3,"INSERT INTO  duel_game_status (duel_game_id,user_id,publish_status,created_at,updated_at) VALUES('$duel_games_id','$user_id','f','$timestamp','$timestamp')");
 pg_close($dbconn3);
 ?>
 
