0d8dd1d2c693341ef1b39f6cb5dc27c64e07ad9c
raghunadh2006ster <raghunadh2006ster@me.com>
2015-09-29 11:25:28 +0530

    Update choose_matches.php

--- choose_matches.php
+++ choose_matches.php
@@ -2,10 +2,9 @@
 require_once('connection.php'); 
 $id = $_GET[id];
 $date = date_create();
-date_add($date, date_interval_create_from_date_string('1 days'));
 $date = $date->format('Y-m-d H:m:s'); 
 echo $date;
-$result = pg_query($dbconn3, "select id,name from matches where competition_id = '$id' and end_at >= '$date' and published = 't'");
+$result = pg_query($dbconn3, "select * from matches where competition_id = '$id' and start_at >= '$date' and published = 't'");
 $num = pg_numrows($result);
 pg_close();
 $rows = array();
