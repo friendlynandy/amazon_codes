438732399d6fa9c5732fad2ef41439a21ec5eb57
raghunadh2006ster <raghunadh2006ster@me.com>
2015-10-12 12:07:37 +0530

    Update getusername.php

--- getusername.php
+++ getusername.php
@@ -1,6 +1,6 @@
 <?php 
 require_once('connection.php');
-// $username = $_GET["username"];
+ $username = $_GET["username"];
 // $id = $_GET["id"];
 // $result = pg_query($dbconn3, "select username,balance from users where username = '$username'");
 // if(isset($id))
@@ -23,17 +23,19 @@ require_once('connection.php');
 // echo json_encode($rows);
 // }
 // else
-// {
-// $result = pg_query($dbconn3, "select username,balance from users where username = '$username'");
-// $num = pg_numrows($result);
-// $rows = array();
-// while($r = pg_fetch_assoc($result))
-// {
-// 	$rows[] = $r;
-// }
-// echo json_encode($rows);
-// }
-
+if(isset($username))
+ {
+$result = pg_query($dbconn3, "select username,balance from users where username = '$username'");
+$num = pg_numrows($result);
+$rows = array();
+while($r = pg_fetch_assoc($result))
+{
+	$rows[] = $r;
+}
+echo json_encode($rows);
+ }
+else
+{
 $rank_sql = pg_query($dbconn3, "select id,username,full_name,balance from users order by balance desc");
 $num_rows = pg_numrows($rank_sql);
 $rank_rows = array();
@@ -50,5 +52,6 @@ foreach ($rank_rows as $key => $value)
 
 }
 echo json_encode($rank_rows);
+}
 pg_close($dbconn3);
 ?>
