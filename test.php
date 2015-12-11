b54382719a45eeb14e381a68c3a8e658ff98162c
raghunadh2006ster <raghunadh2006ster@me.com>
2015-12-11 15:37:04 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
-<?php
-$name = "Raghu";
-$place = "Bangalore";
+$name = $_POST['name'];
+$email = $_POST['account'];
+$location = $_POST['location'];
+
 foreach( $name as $key => $n ) {
-  print "The name is ".$n." and email is ".$place[$key].", thank you\n";
+  print "The name is ".$n.", email is ".$email[$key].
+        ", and location is ".$location[$key].". Thank you\n";
 }
-?>
