929bae8a767aec6c8b4c3a7b9ac57b63441e9f7f
raghunadh2006ster <raghunadh2006ster@me.com>
2015-12-11 15:37:59 +0530

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@ $name = $_POST['name'];
 $email = $_POST['account'];
 $location = $_POST['location'];
 
-foreach( $name as $key => $n ) {
-  print "The name is ".$n.", email is ".$email[$key].
-        ", and location is ".$location[$key].". Thank you\n";
+foreach( $name as $key => $n ) 
+{
+   json_encode($name);
 }
