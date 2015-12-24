cac0fff62780dd9078a957a8acbe60dc8926caed
raghunadh2006ster <raghunadh2006ster@me.com>
2015-12-24 16:41:38 +0530

    Update tournament.php

--- tournament.php
+++ tournament.php
@@ -1,5 +1,5 @@
 <?php
 require_once('connection.php');
-$data = pg_query($dbconn3,"select name from competitions where status_tournament = 't'");
+$data = pg_query($dbconn3,"select a.name,a.id from competitions a left join tournaments b on a.id=b.competition_id where b.publish_tournament=");
 echo json_encode($data);
 ?>
