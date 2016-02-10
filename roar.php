383f5e6db3cf382d230d5d8f2680f75c9b47d918
raghunadh2006ster <raghunadh2006ster@me.com>
2016-02-10 13:28:05 +0530

    Update roar.php

--- roar.php
+++ roar.php
@@ -53,18 +53,11 @@ $t = microtime(true);
 $micro = sprintf("%06d",($t - floor($t)) * 1000000);
 $d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
 $timestamp = $d->format("Y-m-d H:i:s.u"); 
-echo $timestamp;
-
 
 // Prints the day, date, month, year, time, AM or PM
 //echo gmdate("Y-m-d H:i:s.u") . "<br>";
-
-
-
-
 //$time = gmmktime();
 //echo date("Y-m-d H:i:s", $time); 
-
 
 require_once('connection.php');
 if(isset($_GET["invitebyemail"]))
