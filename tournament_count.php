ee3b8120529fab3fb432c49f4e87a2bd0f4e7b3d
raghunadh2006ster <raghunadh2006ster@me.com>
2016-03-15 11:01:14 +0530

    Update tournament_count.php

--- tournament_count.php
+++ tournament_count.php
@@ -1,3 +1,11 @@
 <?php
-
+require_once('connection.php');
+$result = pg_query($dbconn3,"select count(a.id) from duel_games a 
+left join matches b on a.gameable_id = b.id
+where a.user_id = 25 and a.status = 'finished' and b.competition_id=125 or a.opponent_id = 25 and a.status = 'finished' and b.competition_id=125 or a.user_id = 25 and a.status = 'accepted' and b.competition_id=125 or a.opponent_id = 25 and a.status = 'accepted' and b.competition_id=125");
+while($r = pg_fetch_assoc($result))
+{
+	$rows[] = $r;
+}
+echo json_encode($rows);
 ?>
