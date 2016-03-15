973955ccc1517fc9397b370ce60a2408f4de8a6f
raghunadh2006ster <raghunadh2006ster@me.com>
2016-03-15 11:06:31 +0530

    Update tournament_count.php

--- tournament_count.php
+++ tournament_count.php
@@ -1,8 +1,10 @@
 <?php
 require_once('connection.php');
+$user_id = $_GET["userid"];
+$competition_id = $_GET["competitionid"];
 $result = pg_query($dbconn3,"select count(a.id) from duel_games a 
 left join matches b on a.gameable_id = b.id
-where a.user_id = 25 and a.status = 'finished' and b.competition_id=125 or a.opponent_id = 25 and a.status = 'finished' and b.competition_id=125 or a.user_id = 25 and a.status = 'accepted' and b.competition_id=125 or a.opponent_id = 25 and a.status = 'accepted' and b.competition_id=125");
+where a.user_id = $user_id and a.status = 'finished' and b.competition_id=$competition_id or a.opponent_id = $user_id and a.status = 'finished' and b.competition_id=$competition_id or a.user_id = $user_id and a.status = 'accepted' and b.competition_id=$competition_id or a.opponent_id = $user_id and a.status = 'accepted' and b.competition_id=$competition_id");
 while($r = pg_fetch_assoc($result))
 {
 	$rows[] = $r;
