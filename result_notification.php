91b078706d02e5cd311f26d11f2fb0c455565147
raghunadh2006ster <raghunadh2006ster@me.com>
2016-03-24 20:05:50 +0530

    Update result_notification.php

--- result_notification.php
+++ result_notification.php
@@ -14,14 +14,18 @@ foreach ($rows as $key => $value)
 		{
 		echo "Key: $key; Value: $value[id]<br />\n";
 		echo "Key: $key; Value: $value[name]<br />\n";
-		// $result = pg_query($dbconn3,"select id from matches where updated_at >= '2016-03-23' and published_result = 't' and published_result_notification = 'f'");
-		// $value = pg_numrows($result);
-		// $rows = array();
-		// while($r = pg_fetch_assoc($result))
-		// {
-		// 	$rows[] = $r;
-		// }
-		
+		$result1 = pg_query($dbconn3,"select a.user_id, b.ios_token_id,b.ios_notification_badge from (
+			select user_id from duel_games where gameable_id = '$value[id]' and gameable_type = 'Match' and status = 'finished'
+			union
+			select opponent_id from duel_games where gameable_id = '$value[id]' and gameable_type = 'Match' and status = 'finished') as a
+			left join push_notifiers b on a.user_id = b.user_id");
+		$value1 = pg_numrows($result1);
+		$rows1 = array();
+		while($r = pg_fetch_assoc($result1))
+		{
+			$rows1[] = $r;
+		}
+		var_dump($rows1);
 		}
 }
 
