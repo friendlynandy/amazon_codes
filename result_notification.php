32376232c3ae9f255f145b71bfa9f7e94c44399b
raghunadh2006ster <raghunadh2006ster@me.com>
2016-03-24 19:25:46 +0530

    Update result_notification.php

--- result_notification.php
+++ result_notification.php
@@ -1,13 +1,14 @@
 <?php
 require_once('connection.php');
-$result = pg_query($dbconn3,"select ios_token_id,ios_notification_badge from push_notifiers where user_id =25 ");
+$result = pg_query($dbconn3,"select ios_token_id,ios_notification_badge from push_notifiers where user_id = '$opponentid' where user_id =25 ");
 $value = pg_numrows($result);
 $rows = array();
 while($r = pg_fetch_assoc($result))
 {
 	$rows[] = $r;
 }
-var_dump($rows);
+
+print_r($rows);
 
 // print_r ($value[0]);
 // print_r ($value1[0]);
