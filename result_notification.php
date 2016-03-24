ee383d0ec9ec9a3755eb5de1eff756291a617f28
raghunadh2006ster <raghunadh2006ster@me.com>
2016-03-24 19:42:43 +0530

    Update result_notification.php

--- result_notification.php
+++ result_notification.php
@@ -1,6 +1,6 @@
 <?php
 require_once('connection.php');
-$result = pg_query($dbconn3,"select id from matches where updated_at >= '2016-03-23' and published_result = 't' and published_result_notification = 'f'");
+$result = pg_query($dbconn3,"select id from matches where updated_at >= '2016-03-24' and published_result = 't' and published_result_notification = 'f'");
 $value = pg_numrows($result);
 $rows = array();
 while($r = pg_fetch_assoc($result))
