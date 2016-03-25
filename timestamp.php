8a0eb3a5eaef3739c549b3550d06ce3ee1ffb89c
friendlynandy <nreddy135@gmail.com>
2016-03-25 17:12:52 +0530

    Update timestamp.php

--- timestamp.php
+++ timestamp.php
@@ -1,13 +1,13 @@
 <?php
 
-// $t = microtime(true);
-// $micro = sprintf("%06d",($t - floor($t)) * 1000000);
-// $d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
+$t = microtime(true);
+$micro = sprintf("%06d",($t - floor($t)) * 1000000);
+$d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
 
-// print $d->format("Y-m-d H:i:s.u"); 
+print $d->format("Y-m-d H:i:s.u"); 
 
-$date = new DateTime();
-echo $date->format('Y-m-d H:i:s.u') . "\n"
+// $date = new DateTime();
+// echo $date->format('Y-m-d H:i:s.u') . "\n"
 
 
 ?>
